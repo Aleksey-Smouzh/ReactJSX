4b80c794b304d6126d47b5d17844af6ca7b8d87a
Aleksey-Smouzh <xmz@tut.by>
2022-10-27 17:21:04 +0200

    change html file(add new)

--- react-jsx/src/App.js
+++ react-jsx/src/App.js
@@ -1,10 +1,17 @@
-
+import Header from './component/Header/Header';
 import './App.css';
-
+import Footer from './component/Footer/Footer';
 function App() {
   return (
     <div className="App">
       
+<Header/>
+
+<Footer/>
+
+
+
+
     </div>
   );
 }
---
+++ react-jsx/src/component/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+      <div className='Header-site-header'>
+          <h1> Utp slogan</h1>
+          
+
+
+    </div>
+  )
+}
